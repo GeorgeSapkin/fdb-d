dc0bb93a1fd24a25d480628db25b698be70fa5c2
George Sapkin <george.sapkin@gmail.com>
2014-11-15 18:57:15 +0100

    Added connect example

---
+++ examples/connect/source/app.d
@@ -0,0 +1,26 @@
+import std.stdio;
+
+import
+    fdb;
+
+void main()
+{
+    "Starting network".writeln;
+    startNetwork;
+
+    try
+    {
+        "Creating cluster".writeln;
+        auto cluster = createCluster;
+
+        "Opening database".writeln;
+        auto db = cluster.openDatabase;
+    }
+    catch (FDBException ex)
+    {
+        ex.writeln;
+    }
+
+    "Stopping network".writeln;
+    stopNetwork;
+}
